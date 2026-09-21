ae9c88ee939266dbdb9884ce48977c3e5b36eb8b
Swati78cs <swatishreya63@gmail.com>
2026-09-22 00:47:03 +0530

    Create Search2DMatrix.java

---
+++ Search2DMatrix.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int row = matrix.length;
+        int col = matrix[0].length;
+        int l = 0;
+        int h = (row * col) -  1;
+        while(l <= h){
+            int mid = (l + h) / 2;
+            int r = mid / col;
+            int c = mid % col;
+            if(matrix[r][c] == target){
+                return true;
+            }
+            else if(matrix[r][c] > target){
+                h = mid - 1;
+            }
+            else{
+                l = mid + 1;
+            }
+        }
+        return false;
+    }
+}
